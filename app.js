11ddb4e4170f5d69ee9b035af6b7a86881411591
jleetaylor2020 <jleetaylor2020@icloud.com>
2018-11-10 12:00:01 -0600

    more lab work

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // #3 
-var firstName = "Jess";
-console.log (firstName);
+//var firstName = "Jess";
+//console.log (firstName);
 
 // #4
-const unitedStates = '50';
-console.log (unitedStates);
+//const unitedStates = '50';
+//console.log (unitedStates);
 
 // #5
-var firstNumber = 4, secondNumber = 5
-console.log (firstNumber + secondNumber);
+//var firstNumber = 4, secondNumber = 5
+//console.log (firstNumber + secondNumber);
 
 // #6 STUCK ---NOT FINISHED ---ASCII Codes Table defines Hex Symbols associated with: integars, Letters, Letter combo, and symbols - charCodeAt()method returns an integer btwn 0-65535
 // const letterName = ''
@@ -20,28 +20,32 @@ console.log (firstNumber + secondNumber);
 // #7 - These are comments to my code to document how it funtions here..
 
 // #8 and #9 - This is the call() method
-function sayHello() {
-    alert('Hello World!');
-}
+//function sayHello() {
+//    alert('Hello World!');
+//}
 
-sayHello ();
+//sayHello ();
 
 //  #10 and #11 Q. Is there a way to place multiple paramenters to be ran in this one? ie checkAge X 4 --I added an if/else statement in here -=-
-function checkAge(name, age) {
-    if (age < 21) {
-        alert(' :( Sorry, ' + name + ' you are not old enough to view this page! ');
+//function checkAge(name, age) {
+ //   if (age < 21) {
+ //       alert(' :( Sorry, ' + name + ' you are not old enough to view this page! ');
      
-    }    
-    else {
-        alert(' :) Yey! ' + name + ' you are of age to view the content on this page!');
-    }
+ //   }    
+  //  else {
+  //      alert(' :) Yey! ' + name + ' you are of age to view the content on this page!');
+  //  }
+//}
+//checkAge('Marvin', 22),
+//checkAge('Charles', 21),
+//checkAge('Abby', 27),
+//checkAge('James', 18),
+//checkAge('John', 17);
+
+//#12 and #13 - use a loop on an array
+var favVeggies = ['green beans', 'spinach', 'squash'];
+for (var i=0; i< favVeggies.length; i++) {
+  console.log(favVeggies[i]); 
 }
-checkAge('Marvin', 22),
-checkAge('Charles', 21),
-checkAge('Abby', 27),
-checkAge('James', 18),
-checkAge('John', 17);
-// -------------
-test 
 
 
