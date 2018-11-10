f562ee752779a74021b72342ba261b31687093b7
jleetaylor2020 <jleetaylor2020@icloud.com>
2018-11-10 13:03:38 -0600

    stuck on 3 questions for this lab

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 //var firstNumber = 4, secondNumber = 5
 //console.log (firstNumber + secondNumber);
 
-// #6 STUCK ---NOT FINISHED ---ASCII Codes Table defines Hex Symbols associated with: integars, Letters, Letter combo, and symbols - charCodeAt()method returns an integer btwn 0-65535
+// #6 STUCK ---EXPLORE LATER ---ASCII Codes Table defines Hex Symbols associated with: integars, Letters, Letter combo, and symbols - charCodeAt()method returns an integer btwn 0-65535
 // const letterName = ''
 // const index = 12;
 // string.charCodeAt(12);
@@ -28,24 +28,31 @@
 
 //  #10 and #11 Q. Is there a way to place multiple paramenters to be ran in this one? ie checkAge X 4 --I added an if/else statement in here -=-
 //function checkAge(name, age) {
- //   if (age < 21) {
- //       alert(' :( Sorry, ' + name + ' you are not old enough to view this page! ');
+   // if (age < 21) {
+     //  alert(' :( Sorry, ' + name + ' you are not old enough to view this page! ');
      
- //   }    
-  //  else {
-  //      alert(' :) Yey! ' + name + ' you are of age to view the content on this page!');
-  //  }
+    //}    
+   // else {
+      //  alert(' :) Yey! ' + name + ' you are of age to view the content on this page!');
+   //}
 //}
 //checkAge('Marvin', 22),
 //checkAge('Charles', 21),
 //checkAge('Abby', 27),
 //checkAge('James', 18),
-//checkAge('John', 17);
+//checkAge('John', 17),
 
 //#12 and #13 - use a loop on an array
-var favVeggies = ['green beans', 'spinach', 'squash'];
-for (var i=0; i< favVeggies.length; i++) {
-  console.log(favVeggies[i]); 
-}
+//var favVeggies = ['green beans', 'spinach', 'squash'];
+//for (var i=0; i< favVeggies.length; i++) {
+//  console.log(favVeggies[i]); 
+//}
+
+//#14 and #15 --- STUCK- EXPLORE LATER on how to create this loop with a function..
+//array.forEach(element => {  
+//});
+
+//#16---STUCK- EXPLORE LATER
 
 
+  
