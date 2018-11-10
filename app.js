43fd556eccc735b80a5a69c5e75cbd1d38203b62
jleetaylor2020 <jleetaylor2020@icloud.com>
2018-11-10 10:37:10 -0600

    working on kitchen lab for js

---
+++ app.js
@@ -0,0 +1,37 @@
+// #3 
+var firstName = "Jess";
+console.log (firstName);
+
+// #4
+const unitedStates = '50';
+console.log (unitedStates);
+
+// #5
+var firstNumber = 4, secondNumber = 5
+console.log (firstNumber + secondNumber);
+
+// #6 STUCK ---NOT FINISHED ---ASCII Codes Table defines Hex Symbols associated with: integars, Letters, Letter combo, and symbols - charCodeAt()method returns an integer btwn 0-65535
+// const letterName = ''
+// const index = 12;
+// string.charCodeAt(12);
+// alert ("Back of the line!")
+// alert ("Next!")
+
+// #7 - These are comments to my code to document how it funtions here..
+
+// #8 and #9 - This is the call() method
+function sayHello() {
+    alert('Hello World!');
+}
+
+sayHello ();
+//  #10
+function checkAge(name, age) {
+    if (age < 21) {
+        alert(' Sorry ' + name + ' , you are not old enough to view this page! ');
+        
+    }
+    
+}
+
+checkAge('Marvin', 15);
